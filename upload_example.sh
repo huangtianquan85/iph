abcb003a30457a678cd334c185663a97a89576e7
Megrez <68074108@qq.com>
2022-02-10 16:08:53 +0800

    optimize
    (cherry picked from commit 1eb19fe3d302fa76174435198a9c144acf5c4781)

--- upload_example.sh
+++ upload_example.sh
@@ -1,4 +1,6 @@
 prefix=`cd upload && ls last-*`
 prefix=${prefix%-20*}
-rclone delete tencent-cos:/iph-1258787308 --include "$prefix*" || 0
+if [ -n "$prefix" ]; then 
+    rclone delete tencent-cos:/iph-1258787308 --include "$prefix*" || 0
+fi
 rclone copy upload/ tencent-cos:/iph-1258787308 --progress
