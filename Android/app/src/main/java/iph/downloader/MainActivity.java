bb8cedf1523538eb1ed8633db64a95d2be6802db
Megrez <huangtianquan@gameley.cn>
2022-02-21 10:56:30 +0800

    WebView

--- Android/app/src/main/java/iph/downloader/MainActivity.java
+++ Android/app/src/main/java/iph/downloader/MainActivity.java
@@ -2,13 +2,55 @@ package iph.downloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private int backCount = 0;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.qq.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        if (backCount < 1) {
+            backCount++;
+            Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    backCount = 0;
+                }
+            }, 2000, 2000);
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
