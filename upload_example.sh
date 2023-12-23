c3bcdab2f1a53358696e49e388b84dd82a3a2552
Megrez <huangtianquan@yiduo.link>
2023-12-23 13:30:16 +0800

    optimize upload example

--- upload_example.sh
+++ upload_example.sh
@@ -1,6 +1,7 @@
-prefix=`cd upload && ls last-*`
+# usage: <upload.sh path> <remote_name:bucket_name>
+
 prefix=${prefix%-20*}
 if [ -n "$prefix" ]; then 
-    rclone delete tencent-cos:/iph-1258787308 --include "$prefix*" || 0
+    <rclone_path> delete $1 --include "$prefix*" || 0
 fi
-rclone copy upload/ tencent-cos:/iph-1258787308 --progress
+<rclone_path> copy upload/ $1 --progress
